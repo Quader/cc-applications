5f16d40684689f871a2937d29bc6a46eb94897c2
Steven David <roccobruehl@gmail.com>
2014-09-08 01:35:43 +0200

    Client / Server authentication through json objects and rednet.send()

--- src/auth/authClient.lua
+++ src/auth/authClient.lua
@@ -1,30 +1,56 @@
 rednet.open("top")
 os.pullEvent = os.pullEventRaw
+os.loadAPI("json")
+local serverID = 1087
 
 function playerDetected(player)
-    term.clear()
-    term.setCursorPos(1,1)
-    print("Player Detected: "..player)
+  term.clear()
+  term.setCursorPos(1,1)
+  print("Player Detected: "..player)
 end
 
 function checkPlayerEvent(e)
-    local event = e
-    if event == "player" then
-        print("PlayerEvent detected!")
-        return 1
-    else 
-        print("No PlayerEvent detected!")
-        return 0
-    end
+  local event = e
+  if event == "player" then
+    print("PlayerEvent detected!")
+    return 1
+  else 
+    print("No PlayerEvent detected!")
+    return 0
+  end
 end
 
 while true do
-    print("Waiting for player..")
-    a, b = os.pullEvent(player)
-    
-    local isPlayerEvent = checkPlayerEvent(a)
+  print("Waiting for player..")
+  a, b = os.pullEvent(player)
+  
+  local isPlayerEvent = checkPlayerEvent(a)
 
-    if isPlayerEvent == 1 then
-        playerDetected(b) 
-    end
+  if isPlayerEvent == 1 then
+    playerDetected(b)
+    sleep(2)
+    getPassword(b)
+  end
 end
+
+function authWithServer(player, password)
+  local table = {}
+  table["name"] = player
+  table["password"] = password
+  local payload = json.encode(table)
+  rednet.send(serverID, payload)
+end
+
+function getPassword(player)
+  pwSet = false
+  term.clear()
+  term.setCursorPos(1,1)
+  print("Hello, "..player.."! /n")
+  print("Please enter your Password")
+  term.setCursorPos(5,1)
+  while pwSet == true do
+    local password = term.read()
+
+  end
+end
+
