afcd9ab9d4ed493f0bd6a38628d4750828b11dd3
Alex <alex.neuhausen@gmail.com>
2014-09-08 20:41:06 +0200

    Code cleanup

---
+++ src/api/peripheral.lua
@@ -0,0 +1,50 @@
+function get ( _sType )
+	local tResults = {}
+	for i, sSide in ipairs( rs.getSides() ) do
+		if peripheral.getType( sSide ) == _sType then
+			table.insert( tResults, sSide )
+		end
+	end
+	if #tResults == 0 then
+		return nil
+	else
+		return tResults
+	end
+end
+
+function force ( _sType )
+	local tResults = {}
+	for i, sSide in ipairs( rs.getSides() ) do
+		if peripheral.getType( sSide ) == _sType then
+			table.insert( tResults, sSide )
+		end
+	end
+
+	if #tResults == 0 then
+		term.clear()
+		print( "Es wurde kein ".. tostring( _sType ) .. " gefunden!" )
+		return nil
+	elseif #tResults == 1 then
+		return peripheral.wrap( tResults[1] )
+	else
+		repeat
+			local bool = false
+			term.clear()
+			print( "Es wurden mehrere " .. tostring( _sType ) .. " gefunden!" )
+			for i, sSide in pairs( p ) do
+				local x, y = term.getCursorPos()
+				term.setCursorPos( 1, y + 1 )
+				term.write("F" .. tostring( i ) .. ": " .. tostring( sSide ))
+			end
+			local event, keycode = os.pullEvent( "key" )
+			for i, side in pairs( p ) do
+				if keycode == ( 58 + i ) then
+					bool = true
+					term.clear()
+					term.setCursorPos( 1, 1 )
+					return peripheral.wrap( sSide )
+				end
+			end
+		until bool
+	end
+end
